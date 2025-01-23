bc99493b41427641ee67217badd0796f02952fb1
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-23 14:24:25 -0500

    added HistoryService vars to post and get reqs

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,26 +1,34 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
+import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
   try {
     const { cityName } = req.body;
+  
+    // TODO: GET weather data from city name
     const weatherData = await WeatherService.getWeatherForCity(cityName);
+
+    // TODO: save city to search history
+    await HistoryService.addCity(cityName);
+
     res.status(200).json(weatherData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server Error' });
   }
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
 });
 
 // TODO: GET search history
 router.get('/history', async (req: Request, res: Response) => {
-  res.send("This is the history get route")
+  // res.send("This is the history get route")
+
+  const history = await HistoryService.getCities();
+
+  res.json(history)
 });
 
 // * BONUS TODO: DELETE city from search history
