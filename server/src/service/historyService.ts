2aec2f500f827eab6adeb47a4bc161c133067489
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-23 14:20:38 -0500

    added async getCities and addCity functions to HistoryService class

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs/promises';
+
 // TODO: Define a City class with name and id properties
 
 // TODO: Complete the HistoryService class
@@ -7,9 +9,28 @@ class HistoryService {
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    const history = await fs.readFile("db/db.json", "utf-8");
+    const parsedHistory = JSON.parse(history);
+
+    return parsedHistory
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+      const data = {
+        name: city,
+        id: Math.random().toString(36).substr(2, 9)
+      }
+
+      const history = await fs.readFile("db/db.json", "utf-8");
+      const parsedHistory = JSON.parse(history);
+
+      parsedHistory.push(data);
+
+      await fs.writeFile("db/db.json", JSON.stringify(parsedHistory, null, 4));
+
+
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
